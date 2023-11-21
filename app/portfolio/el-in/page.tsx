288f01646e9ab527c6bcc49a1a54177b3c9600e9
Joshua Lehman <joshlehman.dev@gmail.com>
2023-11-20 22:41:48 -0500

    updates

---
+++ app/portfolio/el-in/page.tsx
@@ -0,0 +1,344 @@
+"use client";
+
+import React from "react";
+import { motion } from "framer-motion";
+import RoundButton from "../../ui/RoundButton";
+import "../../ui/Portfolio.css";
+
+const PortfolioElectronicInv = () => {
+  return (
+    <main className="main-container">
+      <section>
+        <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{
+            opacity: 1,
+            transition: {
+              duration: 0.75,
+              delay: 0.5,
+            },
+          }}
+        >
+          {/* <img
+            alt="laptop on a gold platform"
+            className="portfolio-hero"
+            src="https://res.cloudinary.com/dv6keahg3/image/upload/q_auto/f_auto/v1697384652/PortfolioSite/Laptop_apple_electronics_inventory_golden_lka0ur.png"
+          /> */}
+        </motion.div>
+      </section>
+      <div className="spacer-small"></div>
+      <section>
+        <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{
+            opacity: 1,
+            transition: {
+              duration: 0.75,
+              delay: 0.5,
+            },
+          }}
+          style={{ justifyContent: "space-between", display: "flex" }}
+        >
+          <span>
+            <RoundButton
+              href={"https://github.com/joshl26/electronics-inventory-frontend"}
+              buttonText={"GIT"}
+              buttonClass={"round-button"}
+              rel=""
+              target=""
+            />
+          </span>
+          <span>
+            <RoundButton
+              buttonText={"LIVE SITE"}
+              buttonClass={"round-button"}
+              href={"https://el-in.ca"}
+              rel=""
+              target=""
+            />
+          </span>
+          <span>
+            <RoundButton
+              buttonText={"UI/UX"}
+              buttonClass={"round-button"}
+              rel=""
+              target=""
+              href=""
+            />
+          </span>
+        </motion.div>
+      </section>
+      <div className="spacer-small"></div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{
+          opacity: 1,
+          transition: {
+            duration: 0.75,
+            delay: 0.5,
+          },
+        }}
+      >
+        <section className="portfolio-main-section">
+          <h2 className="portfolio-h2">DETAILS</h2>
+          <div style={{ justifyContent: "space-between", display: "flex" }}>
+            <span>
+              <h4>SPRING 2023-CURRENT</h4>
+            </span>
+            <span>
+              <h4>COMMMERCIAL PRODUCT</h4>
+            </span>
+          </div>
+          <div style={{ justifyContent: "space-between", display: "flex" }}>
+            <span>
+              <span>
+                <RoundButton
+                  buttonText={"JAVASCRIPT"}
+                  buttonClass={"round-button"}
+                  rel=""
+                  target=""
+                  href=""
+                />
+              </span>
+              <span>
+                <RoundButton
+                  buttonText={"CSS3"}
+                  buttonClass={"round-button"}
+                  rel=""
+                  target=""
+                  href=""
+                />
+              </span>
+              <span>
+                <RoundButton
+                  buttonText={"HTML5"}
+                  buttonClass={"round-button"}
+                  rel=""
+                  target=""
+                  href=""
+                />
+              </span>
+              <span>
+                <RoundButton
+                  buttonText={"WIREFRAMING"}
+                  buttonClass={"round-button"}
+                  rel=""
+                  target=""
+                  href=""
+                />
+              </span>
+              <span>
+                <RoundButton
+                  buttonText={"UI/UX DESIGN"}
+                  buttonClass={"round-button"}
+                  rel=""
+                  target=""
+                  href=""
+                />
+              </span>
+            </span>
+            <span>
+              <span>
+                <RoundButton
+                  buttonText={"REACT"}
+                  buttonClass={"round-button"}
+                  rel=""
+                  target=""
+                  href=""
+                />
+              </span>
+              <span>
+                <RoundButton
+                  buttonText={"REDUX"}
+                  buttonClass={"round-button"}
+                  rel=""
+                  target=""
+                  href=""
+                />
+              </span>
+              <span>
+                <RoundButton
+                  buttonText={"OPENAPI"}
+                  buttonClass={"round-button"}
+                  rel=""
+                  target=""
+                  href=""
+                />
+              </span>
+              <span>
+                <RoundButton
+                  buttonText={"BABEL"}
+                  buttonClass={"round-button"}
+                  rel=""
+                  target=""
+                  href=""
+                />
+              </span>
+              <span>
+                <RoundButton
+                  buttonText={"WEBPACK"}
+                  buttonClass={"round-button"}
+                  rel=""
+                  target=""
+                  href=""
+                />
+              </span>
+              <span>
+                <RoundButton
+                  buttonText={"GIT"}
+                  buttonClass={"round-button"}
+                  rel=""
+                  target=""
+                  href=""
+                />
+              </span>
+            </span>
+          </div>
+          <div className="spacer-small"></div>
+          <p className="portfolio-p">
+            Electronics Inventory (Ei) is a Full featured inventory management
+            and control system designed for small and large businesses alike!
+            Easy to set up and get started, also has the flexibility to scale
+            for corporate environments. Cloud-based architecture under the hood
+            assures you can see your inventory levels from any device, anywhere
+            in the world!
+          </p>
+        </section>
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{
+          opacity: 1,
+          transition: {
+            duration: 0.75,
+            delay: 0.25,
+          },
+        }}
+      >
+        <section className="portfolio-main-section">
+          <h2 className="portfolio-h2">DESIGN INTENT</h2>
+          <p className="portfolio-p">
+            The intent with this application was to deliver key business metrics
+            regarding the lifecycle of electronics inventory within your
+            organization. The organic layout of the app allows the free flow of
+            information, which improves overall efiicinecy of your business. Try
+            a demo to see!
+          </p>
+        </section>
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{
+          opacity: 1,
+          transition: {
+            duration: 0.75,
+            delay: 0.5,
+          },
+        }}
+      >
+        <section className="portfolio-main-section">
+          <h2 className="portfolio-h2">FONT TYPES</h2>
+          <div
+            style={{
+              justifyContent: "space-between",
+              display: "flex",
+              alignItems: "flex-end",
+            }}
+          >
+            <span>
+              <h1 className="portfolio-font-nunito">NUNITO</h1>
+            </span>
+            <div className="portfolio-li">
+              <span>Weight</span>
+              <ul
+                style={{
+                  listStyleType: "none",
+                  display: "flex",
+                  gap: "0.625rem",
+                }}
+              >
+                <li>200</li>
+                <li>300</li>
+                <li>500</li>
+              </ul>
+            </div>
+          </div>
+          <div
+            style={{
+              justifyContent: "space-between",
+              display: "flex",
+              alignItems: "flex-end",
+            }}
+          >
+            <span className="portfolio-font-cabin">CABIN</span>
+            <div className="portfolio-li">
+              <span>Weight</span>
+              <ul
+                style={{
+                  listStyleType: "none",
+                  display: "flex",
+                  gap: "0.625rem",
+                }}
+              >
+                <li>400</li>
+                <li>500</li>
+                <li>600</li>
+                <li>700</li>
+              </ul>
+            </div>
+          </div>
+        </section>
+      </motion.div>
+      <section className="portfolio-main-section">
+        <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{
+            opacity: 1,
+            transition: {
+              duration: 0.75,
+              delay: 0.5,
+            },
+          }}
+        >
+          <h2 className="portfolio-h2">SCREENS</h2>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{
+            opacity: 1,
+            transition: {
+              duration: 0.75,
+              delay: 0.5,
+            },
+          }}
+        >
+          {/* <img
+            alt="laptop in a modern office"
+            className="portfolio-screen"
+            src="https://res.cloudinary.com/dv6keahg3/image/upload/q_auto/f_auto/v1697384653/PortfolioSite/Laptop_apple_electronics_inventory_a1gelp.png"
+          /> */}
+        </motion.div>
+        <div className="spacer"></div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{
+            opacity: 1,
+            transition: {
+              duration: 0.75,
+              delay: 0.5,
+            },
+          }}
+        >
+          {/* <img
+            alt="laptop on a gold platform"
+            className="portfolio-screen"
+            src="https://res.cloudinary.com/dv6keahg3/image/upload/q_auto/f_auto/v1697384652/PortfolioSite/Laptop_apple_electronics_inventory_golden_lka0ur.png"
+          /> */}
+        </motion.div>
+        <div className="spacer"></div>
+      </section>
+    </main>
+  );
+};
+
+export default PortfolioElectronicInv;
---
+++ app/portfolio/page.tsx
@@ -0,0 +1,412 @@
+"use client";
+
+import React from "react";
+// import { Col, Row } from "react-bootstrap";
+import "../ui/Portfolio.css";
+import RoundButton from "../ui/RoundButton";
+import { motion } from "framer-motion";
+import Link from "next/link";
+
+const Portfolio = () => {
+  return (
+    <main className="main-container">
+      <section className="portfolio-section">
+        <h1 className="portfolio-h1">Portfolio</h1>
+      </section>
+      <section className="projects-section">
+        <div>
+          <div>
+            <motion.div
+              initial={{ opacity: 0 }}
+              whileInView={{
+                opacity: 1,
+                transition: {
+                  duration: 0.5,
+                  delay: 0,
+                },
+              }}
+            >
+              <Link href={"/portfolio/el-in"}>
+                <img
+                  className="showcase-card-container"
+                  alt="3d portfolio hero"
+                  src="https://res.cloudinary.com/dv6keahg3/image/upload/q_auto/f_auto/v1697384652/PortfolioSite/Laptop_apple_electronics_inventory_golden_lka0ur.png"
+                />
+                <div className="spacer-small"></div>
+                <div className="spacer-small"></div>
+              </Link>
+              <div className="spacer"></div>
+            </motion.div>
+          </div>
+          <div>
+            <motion.div
+              initial={{ opacity: 0 }}
+              whileInView={{
+                opacity: 1,
+                transition: {
+                  duration: 0.5,
+                  delay: 0,
+                },
+              }}
+              className="showcase-border-top"
+            >
+              <h3 className="showcased-h3">electronics inventory</h3>
+              <p className="showcased-p">
+                Electronics Inventory is a cutting-edge web based app that
+                efficiently organizes electronic lab inventory for both small
+                businesses and individuals alike. The backend API has been fully
+                documented and tested using OpenAPI and Swagger. Electronics
+                Inventory web-app is available on on any device from desktop pc
+                to the smallest handheld. Manage your electronics inventory from
+                anywhere!
+              </p>
+              <div>
+                <div>
+                  <ul className="showcased-list">
+                    <li>JAVASCRIPT</li>
+                    <li>CSS3</li>
+                    <li>HTML5</li>
+                    <li>WIREFRAMING</li>
+                    <li>UI/UX DESIGN</li>
+                    <li>REACT</li>
+                    <li>REDUX</li>
+                  </ul>
+                </div>
+                <div>
+                  <ul className="showcased-list">
+                    <li>OPENAPI</li>
+                    <li>BACKEND DESIGN</li>
+                    <li>EXPRESS</li>
+                    <li>REST API</li>
+                    <li>BABEL</li>
+                    <li>WEBPACK</li>
+                    <li>GIT</li>
+                  </ul>
+                </div>
+              </div>
+              <div>
+                <RoundButton
+                  buttonClass={"round-button"}
+                  buttonText={"FRONTEND CODE"}
+                  href="https://github.com/joshl26/electronics-inventory-frontend"
+                  target=""
+                  rel=""
+                />
+                <RoundButton
+                  buttonClass={"round-button"}
+                  buttonText={"BACKEND CODE"}
+                  href="https://github.com/joshl26/electronics-inventory-backend"
+                  target=""
+                  rel=""
+                />
+                <RoundButton
+                  buttonClass={"round-button"}
+                  buttonText={"LIVE SITE"}
+                  href="https://el-in.ca/"
+                  target=""
+                  rel=""
+                />
+                <Link href={"/portfolio/el-in/"}>
+                  <RoundButton
+                    buttonClass={"round-button"}
+                    buttonText={"SEE MORE"}
+                    href=""
+                    target=""
+                    rel=""
+                  />
+                </Link>
+              </div>
+            </motion.div>
+          </div>
+        </div>
+        <div className="spacer-small"></div>
+
+        <div>
+          <div>
+            <motion.div
+              initial={{ opacity: 0 }}
+              whileInView={{
+                opacity: 1,
+                transition: {
+                  duration: 0.5,
+                  delay: 0,
+                },
+              }}
+            >
+              <Link href={"/portfolio/pomodor"}>
+                <img
+                  className="showcase-card-container"
+                  alt="pomodor timer hero"
+                  src="https://res.cloudinary.com/dv6keahg3/image/upload/q_auto/f_auto/v1697384650/PortfolioSite/Iphone_horizontal_and_vertical_pomo_vases_ng2hpt.png"
+                />
+                <div className="spacer-small"></div>
+                <div className="spacer-small"></div>
+              </Link>
+              <div className="spacer"></div>
+            </motion.div>
+          </div>
+          <div>
+            <motion.div
+              initial={{ opacity: 0 }}
+              whileInView={{
+                opacity: 1,
+                transition: {
+                  duration: 0.5,
+                  delay: 0,
+                },
+              }}
+              className="showcase-border-top"
+            >
+              <h3 className="showcased-h3">Pomodor Timer</h3>
+              <p className="showcased-p">
+                This project was started in winter 2023 as an introduction to
+                ReactJS. The goal was to explore basic and advanced topics such
+                as prop drilling, React Router, responsive UI/UX design, and
+                state management with Redux.
+              </p>
+              <div>
+                <div>
+                  <ul className="showcased-list">
+                    <li>JAVASCRIPT</li>
+                    <li>BOOTSTRAP</li>
+                    <li>HTML5</li>
+                    <li>REACT</li>
+                    <li>REDUX</li>
+                  </ul>
+                </div>
+                <div>
+                  <ul className="showcased-list">
+                    <li>UI/UX DESIGN</li>
+
+                    <li>BABEL</li>
+                    <li>WEBPACK</li>
+                    <li>GIT</li>
+                  </ul>
+                </div>
+              </div>
+              <div>
+                <RoundButton
+                  buttonClass={"round-button"}
+                  buttonText={"CODE"}
+                  href={"https://github.com/joshl26/pomodoro-app"}
+                  target=""
+                  rel=""
+                />
+                <RoundButton
+                  buttonClass={"round-button"}
+                  buttonText={"LIVE SITE"}
+                  href={"https://joshlehman.ca/pomodor"}
+                  target=""
+                  rel=""
+                />
+                <Link href={"/portfolio/pomodor"}>
+                  <RoundButton
+                    buttonClass={"round-button"}
+                    buttonText={"SEE MORE"}
+                    href=""
+                    target=""
+                    rel=""
+                  />
+                </Link>
+              </div>
+              <div className="spacer"></div>
+            </motion.div>
+          </div>
+        </div>
+        <div>
+          <div>
+            <motion.div
+              initial={{ opacity: 0 }}
+              whileInView={{
+                opacity: 1,
+                transition: {
+                  duration: 0.5,
+                  delay: 0,
+                },
+              }}
+            >
+              <Link href={"/portfolio/3dportfolio"}>
+                {/* <img
+                  className="showcase-card-container"
+                  alt="3d portfolio hero"
+                  src="https://res.cloudinary.com/dv6keahg3/image/upload/q_auto/f_auto/v1697384654/PortfolioSite/Iphone_3dportfolio_modern_desktop_1_unmjbe.png"
+                /> */}
+                <div className="spacer-small"></div>
+                <div className="spacer-small"></div>
+              </Link>
+              <div className="spacer"></div>
+            </motion.div>
+          </div>
+          <div>
+            <motion.div
+              initial={{ opacity: 0 }}
+              whileInView={{
+                opacity: 1,
+                transition: {
+                  duration: 0.5,
+                  delay: 0,
+                },
+              }}
+              className="showcase-border-top"
+            >
+              <h3 className="showcased-h3">3D Portfolio</h3>
+              <p className="showcased-p">
+                An experimental three dimensional portfolio site created with
+                ThreeJS and Blender. I had alot of fun learning new technologies
+                to complete this project. In the end the limitations of
+                responsive styling within this 3d environment meant I had to
+                switch back to a more traditional portfolio website layout to
+                offer improved user accessibility over different devices,
+                specifically older browsers. Still it was a FUN experiment and I
+                do see applications for this type of tech in the future with AR
+                (i.e. Augmented Reality).
+              </p>
+              <div>
+                <div>
+                  <ul className="showcased-list">
+                    <li>JAVASCRIPT</li>
+                    <li>CSS3</li>
+                    <li>HTML5</li>
+                    <li>REACT</li>
+                    <li>REDUX</li>
+                  </ul>
+                </div>
+                <div>
+                  <ul className="showcased-list">
+                    <li>FRONTEND DESIGN</li>
+                    <li>BABEL</li>
+                    <li>WEBPACK</li>
+                    <li>GIT</li>
+                  </ul>
+                </div>
+              </div>
+              <div>
+                <RoundButton
+                  href={"https://github.com/joshl26/portfolio-2023"}
+                  buttonClass={"round-button"}
+                  buttonText={"CODE"}
+                  target=""
+                  rel=""
+                />
+                <RoundButton
+                  buttonClass={"round-button"}
+                  buttonText={"LIVE SITE"}
+                  href={"/portfolio/experimental"}
+                  target=""
+                  rel=""
+                />
+                <Link href={"/portfolio/3dportfolio"}>
+                  <RoundButton
+                    buttonClass={"round-button"}
+                    buttonText={"SEE MORE"}
+                    target=""
+                    rel=""
+                    href=""
+                  />
+                </Link>
+              </div>
+              <div className="spacer"></div>
+            </motion.div>
+          </div>
+        </div>
+        <div>
+          <div>
+            <motion.div
+              initial={{ opacity: 0 }}
+              whileInView={{
+                opacity: 1,
+                transition: {
+                  duration: 0.5,
+                  delay: 0,
+                },
+              }}
+            >
+              <Link href={"/portfolio/outdoorsy"}>
+                {/* <img
+                  className="showcase-card-container"
+                  alt="outdoorsy hero"
+                  src="https://res.cloudinary.com/dv6keahg3/image/upload/q_auto/f_auto/v1697384653/PortfolioSite/Itab_outdoorsy_desert_horizontal_p92vrs.png"
+                /> */}
+                <div className="spacer-small"></div>
+                <div className="spacer-small"></div>
+              </Link>
+              <div className="spacer"></div>
+            </motion.div>
+          </div>
+
+          <div>
+            <motion.div
+              initial={{ opacity: 0 }}
+              whileInView={{
+                opacity: 1,
+                transition: {
+                  duration: 0.5,
+                  delay: 0,
+                },
+              }}
+              className="showcase-border-top"
+            >
+              <h3 className="showcased-h3">outdoorsy</h3>
+              <p className="showcased-p">
+                Outdoorsy is a full-stack website project where logged in users
+                can create and review campsites, canoe routes, hiking trails,
+                etc. Its a community for outdoor enthusiasts to connect, share
+                experiences and plan new ones!
+              </p>
+              <div>
+                <div>
+                  <ul className="showcased-list">
+                    <li>JAVASCRIPT</li>
+                    <li>CSS3</li>
+                    <li>HTML5</li>
+                    <li>WIREFRAMING</li>
+                    <li>UI/UX DESIGN</li>
+                  </ul>
+                </div>
+                <div>
+                  <ul className="showcased-list">
+                    <li>BACKEND DESIGN</li>
+                    <li>EXPRESS</li>
+                    <li>REST API</li>
+                    <li>BABEL</li>
+                    <li>WEBPACK</li>
+                    <li>GIT</li>
+                  </ul>
+                </div>
+              </div>
+              <div>
+                <RoundButton
+                  href={"https://github.com/joshl26/Outdoorsy"}
+                  buttonClass={"round-button"}
+                  buttonText={"CODE"}
+                  target=""
+                  rel=""
+                />
+                <RoundButton
+                  href={"https://outdors.ca"}
+                  buttonClass={"round-button"}
+                  buttonText={"LIVE SITE"}
+                  target=""
+                  rel=""
+                />
+                <Link href={"/portfolio/outdoorsy"}>
+                  <RoundButton
+                    buttonClass={"round-button"}
+                    buttonText={"SEE MORE"}
+                    href=""
+                    target=""
+                    rel=""
+                  />
+                </Link>
+              </div>
+              <div className="spacer"></div>
+            </motion.div>
+          </div>
+        </div>
+        <div className="spacer-small"></div>
+      </section>
+    </main>
+  );
+};
+
+export default Portfolio;
