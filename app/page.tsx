ce17508f65d7ab5ccaca5f08c5b43dae097a201f
Joshua Lehman <joshlehman.dev@gmail.com>
2023-11-20 20:42:35 -0500

    refactor

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,8 @@
-"use client";
-
-import { useState } from "react";
 import ResponsiveFooter from "./ui/ResponsiveFooter";
 import ResponsiveHeader from "./ui/ResponsiveHeader";
 import ResponsiveMenu from "./ui/ResponsiveMenu";
 
 export default function Home() {
-  const [section, setSection] = useState(0);
-  const [menuOpened, setMenuOpened] = useState(false);
-  // const [menuClicked, setMenuClicked] = useState(false);
-  // const hamburgerMenuClicked = () => {
-  //   setMenuClicked(!menuClicked);
-  // };
-
   return (
     <main>
       <ResponsiveHeader />
