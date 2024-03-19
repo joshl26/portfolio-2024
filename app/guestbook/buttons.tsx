b0778cf541cf7361fe959364964def27ddd6fe7d
Joshua Lehman <joshlehman.dev@gmail.com>
2024-03-18 21:45:20 -0400

    updates

--- app/guestbook/buttons.tsx
+++ app/guestbook/buttons.tsx
@@ -1,6 +1,7 @@
-'use client';
+"use client";
 
-import { signIn, signOut } from 'next-auth/react';
+import { signIn, signOut } from "next-auth/react";
+import Image from "next/image";
 
 export function SignOut() {
   return (
@@ -17,9 +18,9 @@ export function SignIn() {
   return (
     <button
       className="px-3 py-2 border border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 rounded p-1 text-sm inline-flex items-center leading-4 text-neutral-900 dark:text-neutral-100 mb-8"
-      onClick={() => signIn('github')}
+      onClick={() => signIn("github")}
     >
-      <img alt="GitHub logo" src="/github-logo.svg" width="20" height="20" />
+      <Image alt="GitHub logo" src="/github-logo.svg" width="20" height="20" />
       <div className="ml-3">Sign in with GitHub</div>
     </button>
   );
