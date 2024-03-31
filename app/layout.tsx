c4755e78cb81a7b49962d3437db94e639ae690cc
Joshua Lehman <joshlehman.dev@gmail.com>
2024-03-31 12:34:49 -0400

    updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -76,8 +76,8 @@ export const metadata: Metadata = {
     card: "summary_large_image",
   },
   verification: {
-    // google: "eZSdmzAXlLkKhNJzfgwDqWORghxnJ8qR9_CHdAh5-xw",
-    // yandex: "14d2e73487fa6c71",
+    // google: "",
+    // yandex: "",
   },
 };
 
