19e603e9edb025242572761baf9e8c9743aadf99
Joshua Lehman <joshlehman.dev@gmail.com>
2024-04-27 20:45:29 -0400

    updates

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -8,21 +8,21 @@ export default function sitemap(): MetadataRoute.Sitemap {
   }));
 
   let routes = [
-    "/",
-    "/about",
-    "/blog",
-    "/contact",
-    "/guestbook",
-    "/portfolio",
-    "/portfolio/e-commerce/beanbag-coffee-co",
-    "/portfolio/e-commerce/cleaner-by-design",
-    "/portfolio/el-in",
-    "/portfolio/outdoorsy",
-    "/portfolio/pomodor",
-    "/portfolio/resume-wrangler",
-    "/portfolio/3dportfolio",
+    "",
+    "about",
+    "blog",
+    "contact",
+    "guestbook",
+    "portfolio",
+    "portfolio/e-commerce/beanbag-coffee-co",
+    "portfolio/e-commerce/cleaner-by-design",
+    "portfolio/el-in",
+    "portfolio/outdoorsy",
+    "portfolio/pomodor",
+    "portfolio/resume-wrangler",
+    "portfolio/3dportfolio",
   ].map((route) => ({
-    url: `${process.env.DEPLOYMENT_URL}${route}`,
+    url: `${process.env.DEPLOYMENT_URL}/${route}`,
     lastModified: new Date().toISOString().split("T")[0],
   }));
 
