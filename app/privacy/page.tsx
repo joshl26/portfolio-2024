07911886c6285c07e3a3bb922b613c752c867804
Joshua Lehman <joshlehman.dev@gmail.com>
2024-09-20 15:53:54 -0400

    updates

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -1,5 +1,13 @@
 import React from "react";
 
+export const metadata = {
+  title: "Privacy",
+  description: "View our privacy policy",
+  alternates: {
+    canonical: `/privacy`,
+  },
+};
+
 const Privacy = () => {
   return (
     <div className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
